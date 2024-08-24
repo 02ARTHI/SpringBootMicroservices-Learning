abc6f69c187bd545020419ed7329f77b61f17fd1
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2024-08-24 15:27:14 +0530

    app 2

---
+++ Microservices/rest-template/RestTemplateApp2/src/main/java/com/learning/controller/controllerclass.java
@@ -0,0 +1,25 @@
+package com.learning.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class controllerclass {
+
+	 
+
+
+     @GetMapping("/info-get")	
+	 public String get()
+	 {
+    	 RestTemplate restTemplate = new RestTemplate();
+    	 ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:1111/info", String.class);
+    	 String body = forEntity.getBody();
+    	 return body +" this is rest template app 2";
+	 }
+}
+
