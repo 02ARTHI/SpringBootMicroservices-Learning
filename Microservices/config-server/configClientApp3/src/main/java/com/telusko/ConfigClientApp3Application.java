ee19b30b299281a734ee4f67275155907b4deb74
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2024-08-24 14:03:09 +0530

    config server app 3

---
+++ Microservices/config-server/configClientApp3/src/main/java/com/telusko/ConfigClientApp3Application.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApp3Application.class, args);
+	}
+
+}
